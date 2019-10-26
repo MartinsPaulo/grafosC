885deb5ed8d5f66be9cca11fa8962f6715bbe137
MartinsPaulo <martinspaulo@fc.unesp.br>
2019-10-26 16:18:50 -0300

    Update grafo-lista-adj.c

--- grafo-lista-adj.c
+++ grafo-lista-adj.c
@@ -1,12 +1,13 @@
 /******************************************************************************
 GRAFOS - LISTA DE ADJANCENCIA 
-Uma possÌvel represetaÁ„o de grafos
+Uma poss√≠vel represeta√ß√£o de grafos
 Desenvolvido com o objetivo de ensino
 Paulo Martins
 contato: paulo.martins@unesp.br ou pgsm5@hotmail.com 
 *******************************************************************************/
 
-//Este material È complementar, se quiser entender este algoritmo com imagens acesse: 
+//Este material √© complementar, se quiser entender este algoritmo com imagens acesse: 
+//https://medium.com/@paulomartins_10299/grafos-representa%C3%A7%C3%A3o-e-implementa%C3%A7%C3%A3o-f260dd98823d
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,17 +19,17 @@ typedef TIPOPESO;
 typedef struct adjacencia {
     int vertice; // vertice de destino 
     TIPOPESO peso; // peso associado a aresta que leva ao vertice de destino
-    strutc adjacencia *prox; // O prÛximo elemento da lista de adjacencias
+    strutc adjacencia *prox; // O pr√≥ximo elemento da lista de adjacencias
 }ADJACENCIA;
 
 typedef struct vertice {
-    /* Dados armazenados v„o aqui */
-    ADJACENCIA *cab; //possui apenas a cabeÁa da lista de adjacencia (linha 16)
+    /* Dados armazenados v√£o aqui */
+    ADJACENCIA *cab; //possui apenas a cabe√ßa da lista de adjacencia (linha 16)
 }VERTICE;
 
 typedef struct grafo { 
-    int vertice; // numero de vertice total do grafo     /*Apesar de n„o ser nescessario guardar o numero de vertices  e arestas, o fazemos, pois caso quisermos*/
-    int arestas; // numero de arestas totais do grafo  	/*saber desta informaÁ„o, teriamos que percorrer toda a estrutura. Parece melhor perder 8bits de memÛria salvando isto ;)*/
+    int vertice; // numero de vertice total do grafo     /*Apesar de n√£o ser nescessario guardar o numero de vertices  e arestas, o fazemos, pois caso quisermos*/
+    int arestas; // numero de arestas totais do grafo  	/*saber desta informa√ß√£o, teriamos que percorrer toda a estrutura. Parece melhor perder 8bits de mem√≥ria salvando isto ;)*/
     VERTICE *adj; // Arranjo de vertices da estrutura (linha 22)	
 }GRAFO;
 
